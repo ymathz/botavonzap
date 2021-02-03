1103973ab3ddacd1fa461d43181a5ffde2234ffe
ymathz <mhenriqueyt@gmail.com>
2021-02-03 15:24:22 -0400

    First commit

---
+++ src/toinmenu.js
@@ -0,0 +1,12 @@
+const VeiodaAvonmenu = (prefix, pushname) => {
+    return `◪ *Comandos do Veio da Avon*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.toinmenu = toinmenu
